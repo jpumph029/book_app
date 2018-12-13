5db74a4b3da2d3c8862f599ed3c981a1e271ec9e
Joe <pumphrey.joseph@gmail.com>
2018-12-12 19:03:52 -0800

    made the button work on index, fixed thumbnail issue with a tenary opperator

--- public/server.js
+++ public/server.js
@@ -12,14 +12,13 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.get('/', getBook);
-
-
+app.get('/new-search', renderSearchForm);
+app.post('/search-results', getSearchResults);
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-app.post('/search', getSearchResults);
 
 // function saveBook(book) {
 //   const SQL = `INSERT INTO books (author,title,isbn,image_url,description,bookshelf) VALUES ($1, $2, $3, $4, $5, $6);`;
@@ -29,13 +28,12 @@ app.post('/search', getSearchResults);
 // }
 
 function Book(data) {
-  // TODO: fix this so || works
   this.author = data.volumeInfo.authors || 'N/A';
   this.title = data.volumeInfo.title || 'N/A';
-  this.isbn = data.isbn;
-  this.thumbnail = data.volumeInfo.imageLinks.thumbnail || 'https://via.placeholder.com/128x200.png';
+  this.isbn = data.isbn || 'N/A';
+  this.thumbnail = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/128x200.png';
   this.description = data.volumeInfo.description || 'N/A';
-  this.bookshelf = data.bookshelf;
+  this.bookshelf = data.bookshelf || 'N/A';
 }
 
 function getSearchResults(req, res) {
@@ -51,22 +49,25 @@ function getSearchResults(req, res) {
     .catch(error => handleError(error));
 }
 
+function getBook(req, res){
+  let sql = 'SELECT * FROM books ';
+  let values=(req.params.books_id);
+  var counter = 0;
+  return client.query(sql,values).then(result =>{
+    result.rows.forEach( () => counter++);
+    res.render('pages/index', {books: result.rows, numBooks: counter});
+  }).catch((err) =>console.log(err.message));
+}
+
+function renderSearchForm(req, res){
+  res.render('./pages/searches/new');
+}
+
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
 
 function handleError(err, res) {
   console.error(err);
-  if (err) res.satus(500).render('./pages/error');
-}
-
-function getBook(req, res){
-  let sql = 'SELECT * FROM books ';
-  let values=(req.params.books_id);
-  var counter = 0;
-  return client.query(sql,values).then(result =>{
-    result.rows.forEach( row => counter++);
-    console.log(counter);
-    res.render('pages/index', {books: result.rows, numBooks: counter});
-  }).catch((err) =>console.log(err.message));
+  if (err) res.status(500).render('./pages/error');
 }
